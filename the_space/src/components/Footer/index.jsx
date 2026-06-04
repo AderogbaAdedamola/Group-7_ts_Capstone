449f42b4018d290966ae26422c0111fdaa495486
Adedamola <aderogbaadedamola8@gmail.com>
2026-06-04 23:09:09 +0100

    Added Footer to the project

--- the_space/src/components/Footer/index.jsx
+++ the_space/src/components/Footer/index.jsx
@@ -1,7 +1,44 @@
-
+import "./index.css"
 
 export default function Footer() {
+  const members = [
+    { initial: "A", name: "Adedamola", colorClass: "avatar-a" },
+    { initial: "P", name: "Peter", colorClass: "avatar-m" },
+    { initial: "B", name: "Balogun", colorClass: "avatar-i" },
+    { initial: "A", name: "Awoniran", colorClass: "avatar-m" },
+    { initial: "R", name: "Rashidat", colorClass: "avatar-m" },
+  ]
+
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-content">
+        <p className="about-title">About</p>
+        <p className="members-title">Members</p>
+        <div className="avatar-stack">
+          {members.map((m, index) => (
+            <div key={index} className={`avatar ${m.colorClass}`}>
+              {m.initial}
+            </div>
+          ))}
+        </div>
+        <ol className="members-list">
+          {members.map((m, index) => (
+            <li key={index}>{m.name}</li>
+          ))}
+        </ol>
+      </div>
+
+      <hr />
+
+      <div className="copyright">
+        <div className="copyright-content">
+          <p>&copy;2026 Designed by Amaka &amp; Ifeoma A.</p>
+          <p>Built by GroupName. All rights reserved</p>
+        </div>
+        <div className="tsacademy">
+          <p>TSAcademy</p>
+        </div>
+      </div>
+    </div>
   )
 }
