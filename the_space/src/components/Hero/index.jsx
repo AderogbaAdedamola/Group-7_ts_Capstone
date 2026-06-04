646b1f6f5dc1857aa50d3432802582c2535267d4
Oyinkansola04 <rashidatomowunmi71@gmail.com>
2026-06-04 19:50:20 +0100

    Add Hero component

--- the_space/src/components/Hero/index.jsx
+++ the_space/src/components/Hero/index.jsx
@@ -1,6 +1,23 @@
-
+import "./index.css";
+import heroImage from "../../assets/image.png";
 export default function Hero() {
   return (
-    <div>Hero is hero</div>
+    <section className="hero">
+      <div className="hero-container">
+        <div className="hero-content">
+          <h1 className="hero-title">Explore Our Solar System Through Data</h1>
+          <p className="hero-description">
+            Understand the planets not just by name, but by measurable facts. From size and mass to gravity and density, this page breaks down the solar system in a clear, data-driven way.
+          </p>
+          <div className="hero-buttons">
+            <button className="hero-button explore" onClick={}>Explore the Data</button>
+            <button className="hero-button contact">Contact Us</button>
+          </div>
+        </div>
+        <div className="hero-image-container">
+          <img src={heroImage} alt="Hero" className="hero-image" />
+        </div>
+      </div>
+    </section>
   )
 }
